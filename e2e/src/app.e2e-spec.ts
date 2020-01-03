cd9b5db52f2f6fb593f620607d1dded2068499ff
Pankaj Pede <pede.pankaj@gmail.com>
2020-01-03 23:23:40 +0530

    build deploy

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('saptagiri app is running!');
+    expect(page.getTitleText()).toEqual('postepodar app is running!');
   });
 
   afterEach(async () => {
